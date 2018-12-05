98676b9a11c791ce0ef4bbda6c9ba3c8a3ce89f8
Paul Nyondo <nyondopaul23@gmail.com>
2018-12-05 15:27:33 +0300

    Initial Commit

---
+++ OfficeChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OfficeChat
+//
+//  Created by Paul Nyondo on 05/12/2018.
+//  Copyright © 2018 Paul Nyondo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
